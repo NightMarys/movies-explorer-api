a1fb994f4dbf890752a8cbee813c4b2ccfda4774
Mary <marinakhripankova@gmail.com>
2023-11-15 19:38:58 +0300

    cors 2

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,27 +1,27 @@
 const allowedCors = [
-  "localhost:3000",
-  "https://api.thatsmovies.nomoredomainsrocks.ru/",
-  "http://api.thatsmovies.nomoredomainsrocks.ru/",
-  "https://thatsmovies.nomoredomainsrocks.ru/",
-  "http://thatsmovies.nomoredomainsrocks.ru/",
+  'localhost:3000',
+  'https://api.thatsmovies.nomoredomainsrocks.ru/',
+  'http://api.thatsmovies.nomoredomainsrocks.ru/',
+  'https://thatsmovies.nomoredomainsrocks.ru/',
+  'http://thatsmovies.nomoredomainsrocks.ru/',
 ];
 
 module.exports = (req, res, next) => {
-  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Credentials', true);
 
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
+    res.header('Access-Control-Allow-Origin', origin);
   }
 
-  if (req.method === "OPTIONS") {
+  if (req.method === 'OPTIONS') {
     res.header(
-      "Access-Control-Allow-Methods",
-      "GET,HEAD,PUT,PATCH,POST,DELETE"
+      'Access-Control-Allow-Methods',
+      'GET,HEAD,PUT,PATCH,POST,DELETE',
     );
     res.header(
-      "Access-Control-Allow-Headers",
-      req.headers["access-control-request-headers"]
+      'Access-Control-Allow-Headers',
+      req.headers['access-control-request-headers'],
     );
     return res.end();
   }
