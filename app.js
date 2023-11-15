c00a264f157071813b46443905784f19fe3eb51d
Mary <marinakhripankova@gmail.com>
2023-11-15 20:52:01 +0300

    cors

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const mongoose = require('mongoose');
 
-const cors = require('./middlewares/cors');
+// const cors = require('./middlewares/cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes/index');
 const { MONGO_URL_DEV } = require('./utils/constants');
@@ -13,11 +13,13 @@ const { NODE_ENV, MONGO_URL } = process.env;
 const { PORT = 3000 } = process.env;
 
 const app = express();
-app.use(cors);
+
 app.use(express.json());
 mongoose.connect(NODE_ENV === 'production' ? MONGO_URL : MONGO_URL_DEV);
 
 app.use(requestLogger);
+
+// app.use(cors);
 
 app.use(router);
 app.use(errorLogger);
